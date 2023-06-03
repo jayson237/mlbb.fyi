e61333aceb4ad80e5851a971ccdbe426afef1d34
Jayson Ng <jaysonng3@gmail.com>
2023-06-04 03:18:07 +0800

    add new wiki structure and solve img issue

--- components/wiki/heroes/hero-card.tsx
+++ components/wiki/heroes/hero-card.tsx
@@ -1,31 +1,32 @@
+"use client";
+
 import { Hero } from "@prisma/client";
 import { GradiantCard } from "@/components/shared/gradiant-card";
 import Image from "next/image";
 
 const HeroCard = ({ hero }: { hero: Hero }) => {
   return (
-    <GradiantCard className="w-fit cursor-pointer p-1.5">
-      <div className="relative h-32 w-24 overflow-hidden rounded-2xl">
+    <div className="w-fit cursor-pointer p-1.5">
+      <div className="relative h-32 w-24 overflow-hidden rounded-lg">
         <Image
           src={
             hero.img.split("/image/upload")[0] +
-            "/image/upload/f_auto/q_3" +
-            hero.img.split("/image/upload")[1]
+              "/image/upload/c_fill,h_256,w_192,g_north" +
+              hero.img.split("/image/upload")[1] || "/nana.jpg"
           }
           alt={hero.name}
           width={96}
           height={128}
-          quality={1}
-          className="absolute inset-0 w-full overflow-hidden  rounded-2xl bg-cover bg-top bg-no-repeat"
+          className="h-[128px] w-[96px] overflow-hidden rounded-lg bg-cover bg-top bg-no-repeat"
           loading="lazy"
         />
       </div>
       <div className="relative w-full">
-        <p className="absolute inset-x-0 top-[-28px] mx-1  mt-[6px] rounded-full bg-navy-900/60 p-0.5 text-center text-[10px] font-medium shadow-inner shadow-navy-500/40 backdrop-blur-md">
+        <p className="absolute inset-x-0 top-[-28px] mx-1  mt-[6px] rounded-md bg-navy-900/60 p-0.5 text-center text-[10px] font-medium shadow-inner shadow-navy-500/40">
           {hero.name}
         </p>
       </div>
-    </GradiantCard>
+    </div>
   );
 };
 
--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ export async function middleware(request: NextRequest) {
     }
 
     const get = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/profile/settings/api/bind?email=${token?.email}`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/profile/stg/api/bind?email=${token?.email}`,
       {
         method: "GET",
       }
@@ -30,35 +30,39 @@ export async function middleware(request: NextRequest) {
     } = await get.json();
 
     if (!data.username) {
-      return NextResponse.rewrite(new URL("/profile/settings", request.url));
+      return NextResponse.rewrite(new URL("/profile/stg", request.url));
     }
   }
 
-  if (path.split("/")[1] === "profile" && path.split("/")[2] === "settings") {
-    const token = await getToken({
-      req: request,
-      secret: process.env.NEXTAUTH_SECRET,
-    });
-    const get = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/profile/settings/api/bind?email=${token?.email}`,
-      {
-        method: "GET",
-      }
-    );
-    const data: {
-      email: string;
-      id: string;
-      image?: string;
-      username?: string;
-      accId?: string;
-    } = await get.json();
+  // if (path.split("/")[1] === "profile" && path.split("/")[2] === "settings") {
+  //   const token = await getToken({
+  //     req: request,
+  //     secret: process.env.NEXTAUTH_SECRET,
+  //   });
+  //   const get = await fetch(
+  //     `${process.env.NEXT_PUBLIC_BASE_URL}/profile/settings/api/bind?email=${token?.email}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   );
+  //   const data: {
+  //     email: string;
+  //     id: string;
+  //     image?: string;
+  //     username?: string;
+  //     accId?: string;
+  //   } = await get.json();
 
-    if (data.accId)
-      return NextResponse.redirect(new URL("/profile/settings", request.url));
+  //   if (data.accId)
+  //     return NextResponse.redirect(new URL("/profile/settings", request.url));
+  // }
+
+  if (path.split("/")[1] === "wiki") {
+    return NextResponse.redirect(new URL("/wiki/heroes", request.url));
   }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/profile/settings", "/profile"],
+  matcher: ["/profile/stg", "/profile", "/wiki"],
 };
