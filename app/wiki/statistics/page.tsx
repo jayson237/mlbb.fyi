0f19dfe4116d763f5d7a9b3d5e7a9684f3319b3e
Jayson Ng <jaysonng3@gmail.com>
2023-06-25 23:44:14 +0800

    init stats page

--- app/wiki/statistics/page.tsx
+++ app/wiki/statistics/page.tsx
@@ -1,13 +1,18 @@
+import getHeroes from "@/lib/actions/getHeroes";
+
+import { Hero } from "@prisma/client";
+
 import { TabsContent } from "@/components/shared/tabs";
 import StatsContainer from "@/components/wiki/statistics/stats-container";
 
 async function StatisticsPage() {
+  const heroes: Hero[] | null = await getHeroes();
   return (
     <TabsContent
       value="heroes"
       className="flex w-full flex-col gap-5 md:flex-row"
     >
-      <StatsContainer />
+      <StatsContainer heroes={heroes} />
     </TabsContent>
   );
 }
--- components/wiki/statistics/stats-container.tsx
+++ components/wiki/statistics/stats-container.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Hero } from "@prisma/client";
 import { GradiantCard } from "@/components/shared/gradiant-card";
 
-export default function StatsContainer() {
+export default function StatsContainer({ heroes }: { heroes: Hero[] | null }) {
   return (
-    <GradiantCard className="w-full" variant="clean">
-      Stats
+    <GradiantCard className="grid w-full grid-cols-4" variant="clean">
+      {heroes?.map((hero, i) => (
+        <div key={i}>{hero.name}</div>
+      ))}
     </GradiantCard>
   );
 }
