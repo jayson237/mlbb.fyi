2f3a67f157bd4699292051725a33963ad3be1542
Jayson Ng <jaysonng3@gmail.com>
2023-06-26 01:07:24 +0800

    create stats container

--- components/wiki/statistics/stats-container.tsx
+++ components/wiki/statistics/stats-container.tsx
@@ -8,15 +8,15 @@ export default function StatsContainer({ heroes }: { heroes: Hero[] | null }) {
   return (
     <GradiantCard className="mb-8 grid grid-cols-4 gap-4" variant="clean">
       <div className="mb-4 font-heading text-xl">Hero</div>
-      <div className="mb-4 text-end font-heading text-xl">Winrate</div>
-      <div className="mb-4 text-end font-heading text-xl">Pick</div>
-      <div className="mb-4 text-end font-heading text-xl">Ban</div>
+      <div className="mb-4 text-end font-heading text-xl">Winrate (%)</div>
+      <div className="mb-4 text-end font-heading text-xl">Pick (%)</div>
+      <div className="mb-4 text-end font-heading text-xl">Ban (%)</div>
       {heroes?.map((hero, i) => (
         <React.Fragment key={i}>
           <div className="text-start font-sat">{hero.name}</div>
-          <div className="text-end font-sat">{hero.win}</div>
-          <div className="text-end font-sat">{hero.use}</div>
-          <div className="text-end font-sat">{hero.ban}</div>
+          <div className="text-end font-sat">{hero.win.slice(0, -1)}</div>
+          <div className="text-end font-sat">{hero.use.slice(0, -1)}</div>
+          <div className="text-end font-sat">{hero.ban.slice(0, -1)}</div>
         </React.Fragment>
       ))}
     </GradiantCard>
