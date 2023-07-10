4ffb71103b455b87d54472ca7ac0cc5e967f358e
Jayson Ng <jaysonng3@gmail.com>
2023-07-11 00:04:04 +0800

    handle image error

--- components/wiki/statistics/stats-container.tsx
+++ components/wiki/statistics/stats-container.tsx
@@ -135,11 +135,12 @@ export default function StatsContainer({ serverStats, tourneyStats }: IStats) {
           </div>
 
           {sortedListCopy.map((hero, i) => {
-            console.log(`https://res.cloudinary.com/dvm5vog2j/image/upload/c_fill,h_192,w_192,g_north/v1686210606/mlbb.fyi/hero/${(hero.name
-              ? hero.name
-              : hero.hero
-            ).replace(/[ '\s]/g, "_")}
-        .webp`);
+            console.log(
+              `https://res.cloudinary.com/dvm5vog2j/image/upload/c_fill,h_192,w_192,g_north/v1686210606/mlbb.fyi/hero/${(hero.name
+                ? hero.name
+                : hero.hero
+              ).replace(/[ '\s]/g, "_")}`
+            );
             return (
               <React.Fragment key={i}>
                 <div className="text-start font-sat text-sm md:text-[16px]">
