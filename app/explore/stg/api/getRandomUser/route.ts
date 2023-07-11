e4b61ce2800c64f7e4d782ad6705cca5f6536da3
Jayson Ng <jaysonng3@gmail.com>
2023-07-12 03:10:51 +0800

    configure skip value

--- app/explore/stg/api/getRandomUser/route.ts
+++ app/explore/stg/api/getRandomUser/route.ts
@@ -5,7 +5,8 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   const currentUser = await getCurrentUser();
   const productsCount = await prisma.user.count();
-  const skip = Math.floor(Math.random() * Math.min(productsCount, 10));
+  const skip = Math.floor(Math.random() * Math.min(productsCount - 1, 100));
+  console.log(skip);
 
   try {
     const users = await prisma.user.findMany({
