aa63568c7b08b863f21f9b7eaffb05878e8b1990
Jayson Ng <jaysonng3@gmail.com>
2023-07-12 02:16:50 +0800

    add reload feature for getting new random users

---
+++ app/explore/stg/api/getRandomUser/route.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/prismadb";
+import getCurrentUser from "@/lib/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const currentUser = await getCurrentUser();
+  const productsCount = await prisma.user.count();
+  const skip = Math.floor(Math.random() * Math.min(productsCount - 1, 10));
+
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        id: {
+          not: {
+            in: [
+              ...(currentUser?.id ? [currentUser.id] : []),
+              ...(currentUser?.following || []),
+            ],
+          },
+        },
+        username: {
+          not: null,
+        },
+      },
+      take: 5,
+      skip: skip,
+      orderBy: {
+        name: "desc",
+      },
+    });
+    // console.log(users);
+    return NextResponse.json(users, {
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Unable to generate random users",
+        error,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
