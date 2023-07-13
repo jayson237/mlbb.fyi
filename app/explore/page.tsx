d43121339001a0a9c98e1ec7beb41d33ccff1e48
Jayson Ng <jaysonng3@gmail.com>
2023-07-13 10:01:54 +0800

    lemonggggg

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -4,43 +4,25 @@ import prisma from "@/lib/prismadb";
 import RandomUserContainer from "@/components/explore/random-user-container";
 import PostListContainer from "@/components/explore/post/post-list-container";
 
-async function getRandomUser() {
-  const currentUser = await getCurrentUser();
-  const productsCount = await prisma.user.count();
-  const skip = Math.floor((Math.random() * productsCount) / 2);
-
-  const users = await prisma.user.findMany({
-    where: {
-      id: {
-        not: {
-          in: [
-            ...(currentUser?.id ? [currentUser.id] : []),
-            ...(currentUser?.following || []),
-          ],
-        },
-      },
-      username: {
-        not: null,
-      },
-    },
-    take: 5,
-    skip: skip,
-    orderBy: {
-      name: "desc",
-    },
-  });
-
-  return users;
+async function getInitialRandomUser() {
+  const res = await fetch(
+    `${process.env.NEXTAUTH_URL}/explore/stg/api/getRandomUser`,
+    {
+      method: "GET",
+      cache: "no-store",
+    }
+  );
+  return res.json();
 }
 
 export default async function ExplorePage() {
   const currentUser = await getCurrentUser();
-  const randomUsers = await getRandomUser();
+  const randomUsers = await getInitialRandomUser();
 
   return (
     <div className="relative flex w-full gap-1.5">
       <PostListContainer currentUser={currentUser} />
-      <RandomUserContainer randomUsers={randomUsers} />
+      <RandomUserContainer randomUsers={randomUsers.users} />
     </div>
   );
 }
