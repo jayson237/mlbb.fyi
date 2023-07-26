a6ab0bc7c36fac6110a34e9ce743252105da04ba
Jayson Ng <jaysonng3@gmail.com>
2023-07-27 00:53:49 +0800

    add clarity analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,11 +55,11 @@ export default async function RootLayout({
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "i5eisaebyw");
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "i5eisaebyw");
           `,
         }}
       />
