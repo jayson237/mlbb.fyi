7dca393dd62ee17901078a2fe6cdd91a483027ce
Jayson Ng <jaysonng3@gmail.com>
2023-07-27 00:39:05 +0800

    persist searchTerm

--- components/explore/post/post-list-container.tsx
+++ components/explore/post/post-list-container.tsx
@@ -35,12 +35,9 @@ const PostListContainer: React.FC<PostListContainerProps> = ({
   currentUser,
 }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [tags, setTags] = useState("");
   const [searchTags, setSearchTags] = useState<string>("");
   const [selectedSortMode, setSelectedSortMode] = useState("recent");
   const [selectedTab, setSelectedTab] = useState("Recent");
-  const [tagCharacterCount, setTagCharacterCount] = useState(0);
-  const [isTagInputFocused, setIsTagInputFocused] = useState<boolean>(false);
 
   // Filter
   const { filter, setFilter } = useFilterStore();
@@ -67,9 +64,14 @@ const PostListContainer: React.FC<PostListContainerProps> = ({
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedOption = Number(event.target.value);
     setSelectedOption(selectedOption);
-    setSearchTerm("");
-    setSearchTags("");
-    setFilter("");
+
+    if (selectedOption === -2) {
+      setSearchTags(searchTerm);
+      setFilter("");
+    } else {
+      setSearchTags("");
+      setFilter(searchTerm);
+    }
   };
 
   const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -77,7 +79,6 @@ const PostListContainer: React.FC<PostListContainerProps> = ({
     setSelectedSortMode(selectedSort);
   };
 
-  // console.log(searchTags);
   return (
     <div className="no-scrollbar max-h-[90vh] w-full overflow-scroll md:w-[2000px]">
       <div className="mb-2">
@@ -109,18 +110,9 @@ const PostListContainer: React.FC<PostListContainerProps> = ({
                 onChange={(e) => {
                   const inputValue = e.target.value;
                   setSearchTerm(inputValue);
-                  if (selectedOption === -2) {
-                    setTagCharacterCount(inputValue.length);
-                  }
                 }}
                 className="flex h-9 rounded-l-xl border-r border-hidden"
                 maxLength={selectedOption === -2 ? 20 : 50}
-                onFocus={() =>
-                  selectedOption === -2 && setIsTagInputFocused(true)
-                }
-                onBlur={() =>
-                  selectedOption === -2 && setIsTagInputFocused(false)
-                }
               />
               <button>
                 <Search className="mr-2 transition-all hover:text-navy-300 hover:duration-300" />
