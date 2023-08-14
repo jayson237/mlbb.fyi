11ee470fbd824325787996173385ab51a30f902a
Jayson Ng <jaysonng3@gmail.com>
2023-08-14 15:18:13 +0800

    fix uskfjanskjndaksjn

---
+++ updateHero.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import * as fs from "fs/promises";
+const prisma = new PrismaClient();
+
+async function main() {
+  // const heroes = await prisma.hero.findMany();
+  const fileData = await fs.readFile("tier-list.json", "utf-8"); // Read the JSON file
+  const itemsData = JSON.parse(fileData);
+
+  for (const hero of itemsData) {
+    const updatedHero = await prisma.hero.update({
+      where: {
+        name: hero.name,
+      },
+      data: {
+        tier: hero.tier,
+      },
+    });
+
+    console.log(updatedHero.name);
+  }
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
